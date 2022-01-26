6d3ebd0d3dd8fbbd7a6dfe2988b297aa781efb7f
hunjong <hunjong@korea.ac.kr>
2022-01-26 14:02:30 +0900

    Modify Setting.sh

--- setting.sh
+++ setting.sh
@@ -28,6 +28,13 @@ echo "***************************"
 echo "Install Package Done!"
 echo "***************************"
 
+#git settings
+echo "***************************"
+echo "git config hunjong@korea.ac.kr"
+echo "***************************"
+git config --global user.email "hunjong@korea.ac.kr"
+git config --global user.name "hunjong"
+
 # Install Zsh
 echo "***************************"
 echo "Install Zsh"
@@ -36,16 +43,23 @@ sudo apt-get install zsh
 
 chsh -s $(which zsh) hunjong;
 curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
-git clone https://github.com/zsh-users/zsh-autosuggestions .oh-my-zsh/custom/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git .oh-my-zsh/custom/plugins/zsh-syntax-highlighting
-sed -i "10s/.*/  ZSH_THEME="agnoster"" ~/.zshrc
-sed -i "70s/.*/plugins=(git zsh-syntax-highlighting zsh-autosuggestions)" ~/.zshrcw
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM}/plugins/zsh-syntax-highlighting
+sed -i "11s/.*/  ZSH_THEME="agnoster"/g" ~/.zshrc
+sed -i "73s/.*/plugins=(git zsh-syntax-highlighting zsh-autosuggestions)/g" ~/.zshrc
 echo "alias tm='tmux' 
 alias cl='clear' 
 alias lt='ls -lt'
 alias ta='tmux attach'
 alias td='tmux detach'" >> ~/.zshrc
 
+# Configure Vim"
+echo "***************************"
+echo "Configure Vim"
+echo "***************************"
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+cp .vimrc ~/.vimrc
+vim +PluginINstall +qall
 
 #Docker 
 #git clone https://github.com/docker/docker-install.git;
