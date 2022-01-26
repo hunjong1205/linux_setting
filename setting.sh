0fddb2451e9b5602e3d6d9447b9ca877c54d6962
hunjong <novel@DESKTOP-U3S4EL2.localdomain>
2022-01-26 13:17:23 +0900

    Modify settings.sh

--- setting.sh
+++ setting.sh
@@ -1,12 +1,13 @@
-#!bin/bash
+#! /bin/bash
 
 echo /etc/hosts >> 163.152.162.75	kuserver    #Make Domain Name Service
 
 sudo apt-get update;
 sudo apt-get upgrade;
 
-sudo apt-get install \
-    zsh \
+echo "Install Pacakge"
+sudo apt-get install -y \
+	vim \
     tmux \
     wget \
     apt-transport-https \
@@ -16,15 +17,33 @@ sudo apt-get install \
     lsb-release \
     git \
     tmux \
-    python3-pip \
-    python-pip
+    python3 \
+    python-pip \
+	build-essential
 
-chsh -s /usr/bin/zsh hunjong;
-sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)";
+sudo apt-get install -y net-tools less 
+echo "Install Package Done!"
+
+# Install Zsh
+echo "Install Zsh"
+sudo apt-get install zsh
+
+chsh -s $(which zsh) hunjong;
+curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
+git clone https://github.com/zsh-users/zsh-autosuggestions .oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git .oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+sed -i "10s/.*/  ZSH_THEME="agnoster"" ~/.zshrc
+sed -i "70s/.*/plugins=(git zsh-syntax-highlighting zsh-autosuggestions)" ~/.zshrcw
+echo "alias tm='tmux' \n
+	alias cl='clear' \n
+	alias lt='ls -lt' \n
+	alias ta='tmux attach' \n
+	alias td='tmux detach'" >> ~/.zshrc
+
 
 #Docker 
-git clone https://github.com/docker/docker-install.git;
-curl -fsSL https://get.docker.com -o get-docker.sh;
-sh get-docker.sh;
-rm -rf ../docker-install;
+#git clone https://github.com/docker/docker-install.git;
+#curl -fsSL https://get.docker.com -o get-docker.sh;
+#sh get-docker.sh;
+#rm -rf ../docker-install;
 
