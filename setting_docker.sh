19b0020b724b48fbab893ad3b4f4648dd7d4e35f
hunjong[docker] <hunjong@korea.ac.kr>
2022-02-15 06:07:04 +0000

    add docker

---
+++ setting_docker.sh
@@ -0,0 +1,76 @@
+#! /bin/bash
+
+# Make Domain Name Service
+# echo "163.152.162.75	kuserver" >> /etc/hosts   
+
+apt-get update && apt-get upgrade
+
+echo "***************************"
+echo "Install Pacakge"
+echo "***************************"
+apt-get install -y \
+	vim \
+    tmux \
+    wget \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release \
+    git \
+    tmux \
+    python3 \
+    python-pip \
+	build-essential
+
+apt-get install -y net-tools less 
+echo "***************************"
+echo "Install Package Done!"
+echo "***************************"
+
+#git settings
+echo "***************************"
+echo "git config hunjong@korea.ac.kr"
+echo "***************************"
+git config --global user.email "hunjong@korea.ac.kr"
+git config --global user.name "hunjong[docker]"
+
+# Install Zsh
+echo "***************************"
+echo "Install Zsh"
+echo "***************************"
+apt-get install zsh
+
+chsh -s $(which zsh)
+curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
+/usr/bin/zsh
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM}/plugins/zsh-syntax-highlighting
+sed -i "11s/.*/  ZSH_THEME="agnoster"/g" ~/.zshrc
+sed -i "73s/.*/plugins=(git zsh-syntax-highlighting zsh-autosuggestions)/g" ~/.zshrc
+echo "alias tm='tmux' 
+alias cl='clear' 
+alias lt='ls -lt'
+alias ta='tmux attach'
+alias td='tmux detach'" >> ~/.zshrc
+
+# Configure Vim"
+echo "***************************"
+echo "Configure Vim"
+echo "***************************"
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+cp .vimrc ~/.vimrc
+vim +PluginINstall +qall
+
+#Docker 
+#git clone https://github.com/docker/docker-install.git;
+#curl -fsSL https://get.docker.com -o get-docker.sh;
+#sh get-docker.sh;
+#rm -rf ../docker-install;
+
+echo "***************************"
+echo "Configuration Initial Linux Done"
+echo "***************************"
+echo "Restart zsh"
+echo "***************************"
+
