de2ff4c744d676f0b04e30233f3c6e1ba76a2648
Hunjong <54570653+hunjong1205@users.noreply.github.com>
2022-05-09 14:16:54 +0900

    Create setting_docker.sh

--- setting_docker.sh
+++ setting_docker.sh
@@ -84,7 +84,7 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 cp .vimrc ~/.vimrc
-curl -sL install-node.vercel.app/lts | bash
+curl -sL install-node.vercel.app/lts | zsh
 vim +PluginInstall +qall
 vim +PlugInstall +qall
 vim -c "CocInstall coc-clangd"
