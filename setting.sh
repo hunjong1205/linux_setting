7443037c7622646b60d67df842378688ba1b1c17
Macbook Pro <hunjong1205@gmail.com>
2022-05-13 16:08:51 +0900

    Integrate Shell Command

--- setting.sh
+++ setting.sh
@@ -3,14 +3,22 @@
 # Make Domain Name Service
 # echo "163.152.162.75	kuserver" >> /etc/hosts   
 
-sudo apt-get update && sudo apt-get upgrade
+while true; do
+	read -p "Settings for Docker? [Y/n] " response
+	case $response in
+		[Nn] ) var='sudo'; break;;
+		[Yy] ) var=''; break;;
+	esac
+done
+
+$var apt-get update && $var apt-get upgrade
 
 echo "***************************"
 echo "***************************"
 echo "Install Pacakge"
 echo "***************************"
 echo "***************************"
-sudo apt-get install -y \
+$var apt-get install -y \
     vim \
     tmux \
     wget \
@@ -30,11 +38,11 @@ sudo apt-get install -y \
 	software-properties-common
 
 # Upgrade Vim version > 8.2
-sudo add-apt-repository -y ppa:jonathonf/vim
-sudo apt-get update -y
-sudo apt-get upgrade -y vim
+$var add-apt-repository -y ppa:jonathonf/vim
+$var apt-get update -y
+$var apt-get upgrade -y vim
 
-sudo apt-get install -y net-tools less 
+$var apt-get install -y net-tools less 
 echo "***************************"
 echo "***************************"
 echo "Install Package Done!"
@@ -54,14 +62,19 @@ echo "***************************"
 echo "Install Zsh"
 echo "***************************"
 echo "***************************"
-sudo apt-get install zsh
+$var apt-get install zsh
 
 chsh -s $(which zsh) hunjong
 curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
-git clone https://github.com/zsh-users/zsh-autosuggestions /home/$(whoami)/.oh-my-zsh/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git /home/$(whoami)/.oh-my-zsh/plugins/zsh-syntax-highlighting
-sudo apt-get install -y language-pack-en
-sudo update-locale
+if [ $var -eq 'sudo' ]; then
+	git clone https://github.com/zsh-users/zsh-autosuggestions /home/$(whoami)/.oh-my-zsh/plugins/zsh-autosuggestions
+	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git /home/$(whoami)/.oh-my-zsh/plugins/zsh-syntax-highlighting
+else
+	git clone https://github.com/zsh-users/zsh-autosuggestions /root/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git /root/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+fi
+$var apt-get install -y language-pack-en
+$var update-locale
 sed -i "11s/.*/  ZSH_THEME="agnoster"/g" ~/.zshrc
 sed -i "73s/.*/plugins=(git zsh-syntax-highlighting zsh-autosuggestions)/g" ~/.zshrc
 echo "alias tm='tmux' 
@@ -69,7 +82,6 @@ alias cl='clear'
 alias lt='ls -lt'
 alias ta='tmux attach'
 alias td='tmux detach'
-
 # Auto Suggestion accpet key binding
 bindkey '!' autosuggest-accept
 bindkey "^L" forward-word
